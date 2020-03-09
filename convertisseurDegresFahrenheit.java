29b3a883b519ccc33ea397b599179f065c4ac0de
Neoshark80 <61883742+Neoshark80@users.noreply.github.com>
2020-03-09 18:43:03 +0100

    Update convertisseurDegresFahrenheit.java

--- convertisseurDegresFahrenheit.java
+++ convertisseurDegresFahrenheit.java
@@ -1,7 +1,3 @@
-package Exception;
-
-import java.util.Scanner;
-
 public class convertisseurDegresFahrenheit {
 	public static void main(String[] args) {
 		convertisseur();
@@ -12,30 +8,30 @@ public class convertisseurDegresFahrenheit {
 		double temperatureAConvertir = 0, resultatEnFahrenheit = ((int) temperatureAConvertir * 1.8 + 32),
 				resultatEnDegrés = (((int) temperatureAConvertir - 32) / 1.8);
 		Scanner sc = new Scanner(System.in);
-			while (retry == 'o' || retry == 'O') {
-				System.out
-						.println("Choisissez votre convertion :\n 1_ Degrés -> Fahrenheit \n 2_ Fahrenheit -> Degrés");
-				conversionSouhaité = sc.nextLine().charAt(0);
-				if (conversionSouhaité == '1') {
-					System.out.println("Température à convertir en Fahrenheit");
-					temperatureAConvertir = sc.nextInt();
-					System.out.println(resultatEnFahrenheit);
-					sc.nextLine();
-					System.out.println("Voulez vous re-essayer? (O/N)");
-					retry = sc.nextLine().charAt(0);
-				} else if (conversionSouhaité == '2') {
-					System.out.println("Température à convertir en Celcius");
-					temperatureAConvertir = sc.nextInt();
-					System.out.println(resultatEnDegrés);
-					sc.nextLine();
-					System.out.println("Voulez vous re-essayer? (O/N) ");
-					retry = sc.nextLine().charAt(0);
-				} else {
-					System.out.println("Je ne comprend pas votre réponse");
-					System.out.println("Voulez vous re-essayer? (0/N)");
-					retry = sc.nextLine().charAt(0);
-	
+
+		while (retry == 'o' || retry == 'O') {
+			System.out.println("Choisissez votre convertion :\n 1_ Degrés -> Fahrenheit \n 2_ Fahrenheit -> Degrés");
+			conversionSouhaité = sc.nextLine().charAt(0);
+			if (conversionSouhaité == '1') {
+				System.out.println("Température à convertir en Fahrenheit");
+				temperatureAConvertir = sc.nextInt();
+				System.out.println(resultatEnFahrenheit);
+				sc.nextLine();
+				System.out.println("Voulez vous re-essayer? (O/N)");
+				retry = sc.nextLine().charAt(0);
+			} else if (conversionSouhaité == '2') {
+				System.out.println("Température à convertir en Celcius");
+				temperatureAConvertir = sc.nextInt();
+				System.out.println(resultatEnDegrés);
+				sc.nextLine();
+				System.out.println("Voulez vous re-essayer? (O/N) ");
+				retry = sc.nextLine().charAt(0);
+			} else {
+				System.out.println("Je ne comprend pas votre réponse");
+				System.out.println("Voulez vous re-essayer? (0/N)");
+				retry = sc.nextLine().charAt(0);
+			}
+
 		}
 	}
-}
 }
