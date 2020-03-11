de39d826ad513165c3f9e9f373780904b19ff00d
Neoshark80 <61883742+Neoshark80@users.noreply.github.com>
2020-03-11 17:04:30 +0100

    Add files via upload

--- convertisseurDegresFahrenheit.java
+++ convertisseurDegresFahrenheit.java
@@ -16,32 +16,37 @@ public class convertisseurDegresFahrenheit {
 			conversionSouhaité = sc.nextLine().charAt(0);
 			if (conversionSouhaité == '1') {
 				try {
-				System.out.println("Température à convertir en Fahrenheit");
-				temperatureEnString = sc.nextLine();
-				temperatureAConvertirEnF = Integer.parseInt(temperatureEnString);
-				}catch (InputMismatchException e) {
-					System.out.println("Merci de rentrer un chiffre.");
-					System.out.println("Voulez vous re-essayer? (O/N) ");
-					retry = sc.nextLine().charAt(0);
+					System.out.println("Température à convertir en Fahrenheit");
+					temperatureEnString = sc.nextLine();
+					temperatureAConvertirEnF = Integer.parseInt(temperatureEnString);
+					double resultatEnFahrenheit = ((int) temperatureAConvertirEnF * 1.8 + 32);
+					System.out.println(resultatEnFahrenheit);
+				} catch (NumberFormatException e) {
+					System.out.println("Veuillez entrer une valeur numérique\n");
 				}
-				double resultatEnFahrenheit = ((int) temperatureAConvertirEnF * 1.8 + 32);
-				System.out.println(resultatEnFahrenheit);
 				System.out.println("Voulez vous re-essayer? (O/N)");
 				retry = sc.nextLine().charAt(0);
-
 			} else if (conversionSouhaité == '2') {
-				System.out.println("Température à convertir en Celcius");
-				temperatureEnString = sc.nextLine();
-				temperatureAConvertirEnC = Integer.parseInt(temperatureEnString);
-				double resultatEnDegrés = (temperatureAConvertirEnC - 32) / 1.8;
-				System.out.println(resultatEnDegrés);
+				try {
+					System.out.println("Température à convertir en Celcius");
+					temperatureEnString = sc.nextLine();
+					temperatureAConvertirEnC = Integer.parseInt(temperatureEnString);
+					double resultatEnDegrés = (temperatureAConvertirEnC - 32) / 1.8;
+					System.out.println(resultatEnDegrés);
+				} catch (NumberFormatException e) {
+					System.out.println("Veuillez entrer une valeur numérique\n");
+				}
 				System.out.println("Voulez vous re-essayer? (O/N) ");
 				retry = sc.nextLine().charAt(0);
 			} else {
-				System.out.println("Je ne comprend pas votre réponse");
+				System.out.println("Je ne comprend pas votre réponse\n");
 				System.out.println("Voulez vous re-essayer? (0/N)");
 				retry = sc.nextLine().charAt(0);
 			}
+		}
+		while (retry == 'n' || retry == 'N') {
+			System.out.println("Au revoir");
+			break;
 		}
 	}
 }
